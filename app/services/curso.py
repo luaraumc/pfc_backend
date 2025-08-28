8a60d50f0e2c950c40b9d29a958af44680b2be4a
ElisaAndradeMostafa <1221104057@alunos.umc.br>
2025-08-28 18:50:00 -0300

    [EDITADO] editado todo os nomes para o singular, afim de padronizar o código

---
+++ app/services/curso.py
@@ -0,0 +1,54 @@
+from app.models import Curso, SessionLocal # modelo de tabela definido no arquivo models.py / sessões para executar operações
+
+# ======================= CRUD =======================
+
+# CREATE - Cria um novo curso
+def criar_curso(session, nome, descricao):
+	novo_curso = Curso(nome=nome, descricao=descricao)
+	session.add(novo_curso)
+	session.commit()
+	session.refresh(novo_curso)
+	return novo_curso.id
+
+# READ - Lista todos os curso
+def listar_curso(session):
+	return session.query(Curso).all()
+
+# READ - Busca um curso pelo id
+def buscar_curso_por_id(session, id):
+	return session.query(Curso).filter(Curso.id == id).first()
+
+# UPDATE - Atualiza os dados de um curso existente
+def atualizar_curso(session, id, nome=None, descricao=None, atualizado_em=None):
+	curso = session.query(Curso).filter(Curso.id == id).first()
+	if curso:
+		if nome:
+			curso.nome = nome
+		if descricao:
+			curso.descricao = descricao
+		if atualizado_em:
+			curso.atualizado_em = atualizado_em
+		session.commit()
+	return curso
+
+# DELETE - Remove um curso pelo id
+def deletar_curso(session, id):
+	curso = session.query(Curso).filter(Curso.id == id).first()
+	if curso:
+		session.delete(curso)
+		session.commit()
+	return curso
+
+# ===================== EXEMPLO DE INSERT =====================
+
+session = SessionLocal()
+
+novo_id = criar_curso(
+    session,
+    nome="Sistemas de Informação",
+    descricao="Teste"
+)
+
+print("ID do novo curso:", novo_id)
+
+session.close()
