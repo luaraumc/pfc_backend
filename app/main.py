b4e9136bc2797d5ca37b565c4e41ef0ac44e5980
luaraumc <Lu@140504>
2025-09-13 18:00:04 -0300

    Remove variáveis não utilizadas e simplifica a importação de módulos no arquivo main.py

--- app/main.py
+++ app/main.py
@@ -1,17 +1,8 @@
 # executar no terminal para rodar: python -m uvicorn app.main:app --reload
+
 from fastapi import FastAPI
-from passlib.context import CryptContext
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
-
-kEY_CRYPT = os.getenv('KEY_CRYPT') # chave de criptografia --> Irá pegar a senha do usuário e criptografar com essa chave fazendo com que a senha fique segura no banco de dados
-ALGORITHM = os.getenv('ALGORITHM') # algoritmo de criptografia
-ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv('ACCESS_TOKEN_EXPIRE_MINUTES'))# tempo de expiração do token de acesso (em minutos)
 
 app = FastAPI()
-bcrypt_context = CryptContext(schemes=["bcrypt"], deprecated="auto") # define o esquema de criptografia --> (deprecated=auto) caso o esquema (bcrypt) fique obsoleto, ele irá atualizar automaticamente
 
 from app.routes.authRoutes import authRouter
 from app.routes.usuarioRoutes import usuarioRouter
