d2eb75a9615363397b55f131117c06f34181d562
Luara Meissner UMC <11221103634@alunos.umc.br>
2025-09-15 11:34:13 -0300

    Refactor database setup import and add comment

--- app/services/carreira.py
+++ app/services/carreira.py
@@ -1,8 +1,9 @@
 from app.models import Carreira # modelo de tabela definido no arquivo models.py
-from app.models import setup_database # conexão do banco de dados
+from app.dependencies import setup_database # conexão do banco de dados
 from app.schemas import CarreiraBase, CarreiraOut # schema de entrada e saída
 
-engine, SessionLocal, Base = setup_database() # configuração do banco de dados
+# Inicializa a conexão com o banco de dados
+engine, SessionLocal, Base = setup_database()
 
 """
 model_dump: converte um objeto do schema em um dicionário para criar ou atualizar modelos SQLAlchemy a partir dos dados recebidos
