eb2fc94975c33d2ff19e9ff85fa16d4a57b47c46
Luara Meissner UMC <11221103634@alunos.umc.br>
2025-09-15 11:15:02 -0300

    Enhance comments in main.py for clarity

--- app/main.py
+++ app/main.py
@@ -1,14 +1,16 @@
 # executar no terminal para rodar: python -m uvicorn app.main:app --reload
 
-from fastapi import FastAPI
+from fastapi import FastAPI # classe FastAPI
 
-app = FastAPI()
+app = FastAPI() # instancia da classe FastAPI
 
+# importando as rotas
 from app.routes.authRoutes import authRouter
 from app.routes.usuarioRoutes import usuarioRouter
 from app.routes.carreiraRoutes import carreiraRouter
 from app.routes.cursoRoutes import cursoRouter
 
+# incluindo as rotas na aplicação FastAPI instanciada
 app.include_router(authRouter)
 app.include_router(usuarioRouter)
 app.include_router(carreiraRouter)
