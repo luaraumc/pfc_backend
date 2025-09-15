8dd6df216f5b3bea2dc5e899c954b68c1ad48c44
Luara Meissner UMC <11221103634@alunos.umc.br>
2025-09-15 11:56:48 -0300

    Add comment for database connection initialization

--- app/services/usuario.py
+++ app/services/usuario.py
@@ -2,7 +2,8 @@ from app.models import Usuario # modelo de tabela definido no arquivo models.py
 from app.dependencies import setup_database # conexão do banco de dados
 from app.schemas import UsuarioBase, UsuarioOut # schema de entrada e saída
 
-engine, SessionLocal, Base = setup_database() # configuração do banco de dados
+# Inicializa a conexão com o banco de dados
+engine, SessionLocal, Base = setup_database()
 
 """
 model_dump: converte um objeto do schema em um dicionário para criar ou atualizar modelos SQLAlchemy a partir dos dados recebidos
