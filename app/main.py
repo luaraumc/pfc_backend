f089faeba7faa87846db197f8eb5f4700f83fd45
Luara Meissner UMC <11221103634@alunos.umc.br>
2025-09-25 08:40:23 -0300

    Adicionar CORS middleware

--- app/main.py
+++ app/main.py
@@ -1,8 +1,22 @@
 # executar no terminal para rodar: python -m uvicorn app.main:app --reload
 
 from fastapi import FastAPI # classe FastAPI
+from fastapi.middleware.cors import CORSMiddleware # middleware para permitir requisições de diferentes origens
 
 app = FastAPI() # instancia da classe FastAPI
+
+origins = [
+    "http://localhost:5173",
+    "http://127.0.0.1:5173"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # importando as rotas
 from app.routes.authRoutes import authRouter
@@ -11,7 +25,6 @@ from app.routes.carreiraRoutes import carreiraRouter
 from app.routes.cursoRoutes import cursoRouter
 from app.routes.habilidadeRoutes import habilidadeRouter
 from app.routes.conhecimentoRoutes import conhecimentoRouter
-from app.routes.vagaRoutes import vagaRouter
 
 # incluindo as rotas na aplicação FastAPI instanciada
 app.include_router(authRouter)
@@ -20,4 +33,3 @@ app.include_router(carreiraRouter)
 app.include_router(cursoRouter)
 app.include_router(habilidadeRouter)
 app.include_router(conhecimentoRouter)
-app.include_router(vagaRouter)
