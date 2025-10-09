fdec59532c8fec50436f7db45d96df7555be51f8
Luara Meissner UMC <11221103634@alunos.umc.br>
2025-10-09 12:27:41 -0300

    Adicionar rota Atualizar habilidade

--- app/routes/habilidadeRoutes.py
+++ app/routes/habilidadeRoutes.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter, Depends, HTTPException # cria dependências e exceções HTTP
 from sqlalchemy.orm import Session # cria sessões com o banco de dados
 from app.schemas import HabilidadeOut # schemas para validação de dados
-from app.services.habilidade import listar_habilidades, buscar_habilidade_por_id, deletar_habilidade # serviços relacionados a habilidade
+from app.services.habilidade import listar_habilidades, buscar_habilidade_por_id, atualizar_habilidade, deletar_habilidade # serviços relacionados a habilidade
 from app.dependencies import pegar_sessao, requer_admin
 
 # Inicializa o router
@@ -16,6 +16,19 @@ def listar(session: Session = Depends(pegar_sessao)):
 @habilidadeRouter.get("/{habilidade_id}", response_model=HabilidadeOut)
 def buscar(habilidade_id: int, session: Session = Depends(pegar_sessao)):
 	habilidade = buscar_habilidade_por_id(session, habilidade_id)
+	if not habilidade:
+		raise HTTPException(status_code=404, detail="Habilidade não encontrada")
+	return habilidade
+
+# Atualizar habilidade - AUTENTICADA
+@habilidadeRouter.put("/atualizar/{habilidade_id}", response_model=HabilidadeOut)
+def atualizar(
+	habilidade_id: int,
+	habilidade_data: HabilidadeOut,
+	usuario: dict = Depends(requer_admin),
+	session: Session = Depends(pegar_sessao),
+):
+	habilidade = atualizar_habilidade(session, habilidade_id, habilidade_data)
 	if not habilidade:
 		raise HTTPException(status_code=404, detail="Habilidade não encontrada")
 	return habilidade
