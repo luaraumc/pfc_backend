70448225d3a9b4542494125a7421999dcda7feb4
Elisa Mostafa <elisamostafa1@gmail.com>
2025-10-13 20:25:22 -0300

    Altera q qtd de caracteres no Models

--- app/models.py
+++ app/models.py
@@ -47,7 +47,7 @@ class Habilidade(Base):
 class Conhecimento(Base):
 	__tablename__ = 'conhecimento'
 	id = Column(Integer, primary_key=True, index=True)
-	nome = Column(String(150), unique=True, nullable=False)
+	nome = Column(String(300), unique=True, nullable=False)
 	atualizado_em = Column(DateTime, server_default=func.now(), nullable=False)
 
 # Modelo da tabela "compatibilidade"
