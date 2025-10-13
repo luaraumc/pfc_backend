f3c1d43842e31641b132d91630f1c15f04e8a3b8
luaraumc <Lu@140504>
2025-10-13 18:34:21 -0300

    Adicionar coluna de frequência ao modelo CarreiraHabilidade

--- app/models.py
+++ app/models.py
@@ -117,11 +117,14 @@ class CursoConhecimento(Base):
 class CarreiraHabilidade(Base):
     __tablename__ = 'carreira_habilidade'
     id = Column(Integer, primary_key=True, index=True)
+    frequencia = Column(Integer, nullable=True) # nova coluna para armazenar a frequência
     carreira_id = Column(Integer, ForeignKey('carreira.id', ondelete='CASCADE'), nullable=False)
     habilidade_id = Column(Integer, ForeignKey('habilidade.id', ondelete='CASCADE'), nullable=False)
+	
     __table_args__ = (
         UniqueConstraint('carreira_id', 'habilidade_id', name='uq_carreira_habilidade'),
     )
+
 
 # Modelo da tabela "usuario_habilidade"
 class UsuarioHabilidade(Base):
