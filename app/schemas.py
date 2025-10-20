4c8e04a7852d4f500dba337f0eb56d188e0d3fc8
Luara Meissner UMC <11221103634@alunos.umc.br>
2025-10-20 14:21:37 -0300

    Add schemas de mapeamento

--- app/schemas.py
+++ app/schemas.py
@@ -226,3 +226,18 @@ class ConfirmarNovaSenhaSchema(BaseModel):
     email: str
     codigo: str
     nova_senha: str
+
+class ItemSimples(BaseModel):
+    id: int
+    nome: str
+
+class RelacaoScore(BaseModel):
+    id: int
+    nome: str
+    score: float
+
+class MapaOut(BaseModel):
+    cursos: List[ItemSimples]
+    carreiras: List[ItemSimples]
+    cursoToCarreiras: Dict[int, List[RelacaoScore]]
+    carreiraToCursos: Dict[int, List[RelacaoScore]]
