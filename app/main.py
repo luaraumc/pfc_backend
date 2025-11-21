b8315bda4d8dc344e54d65722e21feecccd779e2
Luara Meissner UMC <11221103634@alunos.umc.br>
2025-11-21 14:13:12 -0300

    Adicionando carreiraHabilidadeRouter, cursoConhecimentoRouter e usuarioHabilidadeRouter

--- app/main.py
+++ app/main.py
@@ -14,21 +14,27 @@ app.add_middleware(
 )
 
 from app.routes.authRoutes import authRouter
-from app.routes.usuarioRoutes import usuarioRouter
+from app.routes.carreiraHabilidadeRoutes import carreiraHabilidadeRouter
 from app.routes.carreiraRoutes import carreiraRouter
+from app.routes.conhecimentoCategoriaRoutes import conhecimentoCategoriaRouter
+from app.routes.conhecimentoRoutes import conhecimentoRouter
+from app.routes.cursoConhecimentoRoutes import cursoConhecimentoRouter
 from app.routes.cursoRoutes import cursoRouter
 from app.routes.habilidadeRoutes import habilidadeRouter
-from app.routes.conhecimentoRoutes import conhecimentoRouter
-from app.routes.vagaRoutes import vagaRouter
-from app.routes.conhecimentoCategoriaRoutes import conhecimentoCategoriaRouter
 from app.routes.mapeamentoRoutes import mapeamentoRouter
+from app.routes.usuarioHabilidadeRoutes import usuarioHabilidadeRouter
+from app.routes.usuarioRoutes import usuarioRouter
+from app.routes.vagaRoutes import vagaRouter
 
 app.include_router(authRouter)
-app.include_router(usuarioRouter)
+app.include_router(carreiraHabilidadeRouter)
 app.include_router(carreiraRouter)
+app.include_router(conhecimentoCategoriaRouter)
+app.include_router(conhecimentoRouter)
+app.include_router(cursoConhecimentoRouter)
 app.include_router(cursoRouter)
 app.include_router(habilidadeRouter)
-app.include_router(conhecimentoRouter)
-app.include_router(vagaRouter)
-app.include_router(conhecimentoCategoriaRouter)
 app.include_router(mapeamentoRouter)
+app.include_router(usuarioHabilidadeRouter)
+app.include_router(usuarioRouter)
+app.include_router(vagaRouter)
